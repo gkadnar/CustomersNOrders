457b4b4403407633868ddb80312fb2edde93bbcb
gkadnar <goran.kadnar@adcon.hr>
2019-03-27 13:32:58 +0100

    Add project files.

---
+++ Custs.Model.Common/ICustomer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custs.Model.Common
+{
+    interface ICustomer
+    {
+        // Primary key property
+        int Id { get; set; }
+
+        // Value properties
+        string Name { get; set; }
+        string Email { get; set; }
+
+        // Navigation properties
+        IList<IOrder> Orders { get; set; }
+    }
+}
---
+++ Custs.Model.Common/IOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Custs.Model.Common
+{
+    interface IOrder
+    {
+        // Primary key property
+        int Id { get; set; }
+
+        // Value properties
+        string Name { get; set; }
+        int Quantity { get; set; }
+        decimal Price { get; set; }
+
+        // Navigation properties
+        ICustomer Customer { get; set; }
+    }
+}
